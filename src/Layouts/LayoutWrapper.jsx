7e4cd4945086c2c0a23932753003395185591599
MuhammadHassan125 <hassanalirajput2004@gmail.com>
2024-11-08 02:33:19 -0800

    userdetails fix it

--- src/Layouts/LayoutWrapper.jsx
+++ src/Layouts/LayoutWrapper.jsx
@@ -1,23 +1,22 @@
-import { Provider } from 'point-fetch-react'
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import { baseURL } from '../Utils/contants'
-import useErrorDialogs from '../Hooks/useErrorDialogs'
+import { Provider } from "point-fetch-react";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { baseURL } from "../Utils/contants";
+import useErrorDialogs from "../Hooks/useErrorDialogs";
 
-
-
-const LayoutWrapper = ({children}) => {
-    const {handleOpenUnAuthorizeModal, handleOpenInternalServer} = useErrorDialogs();
+const LayoutWrapper = ({ children }) => {
+  const { handleOpenUnAuthorizeModal, handleOpenInternalServer } =
+    useErrorDialogs();
   return (
     <Provider
       baseURL={baseURL}
-      authorization={`Bearer ${localStorage.getItem('user-visited-dashboard')}`}
+      authorization={`Bearer ${localStorage.getItem("user-visited-dashboard")}`}
       onUnAuthenticated={handleOpenUnAuthorizeModal}
       onServerError={handleOpenInternalServer}
     >
-          {children}
+      {children}
     </Provider>
-  )
-}
+  );
+};
 
-export default LayoutWrapper
+export default LayoutWrapper;
