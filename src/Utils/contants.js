dc4942be3b439301b661b973b040fd1841d37c21
MuhammadHassan125 <hassanalirajput2004@gmail.com>
2025-01-22 17:33:37 +0500

    ip again change

--- src/Utils/contants.js
+++ src/Utils/contants.js
@@ -2,7 +2,7 @@
 // export const baseURL = "http://192.168.18.196:8001/api";
 
 // server ip
-export const baseURL = "http://87.106.188.142:3008/api";
-export const AnalyzeURL = "http://87.106.188.142:5000";
+export const baseURL = "http://87.106.188.142:80/api";
+export const AnalyzeURL = "http://87.106.188.142:80";
 
 export const token = localStorage.getItem('user-visited-dashboard');
