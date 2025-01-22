c3bbdea10b8bc593a06fe16c243f5ace076e8e0a
MuhammadHassan125 <hassanalirajput2004@gmail.com>
2025-01-22 17:34:09 +0500

    port change

--- src/Utils/contants.js
+++ src/Utils/contants.js
@@ -3,6 +3,6 @@
 
 // server ip
 export const baseURL = "http://87.106.188.142:80/api";
-export const AnalyzeURL = "http://87.106.188.142:80";
+export const AnalyzeURL = "http://87.106.188.142:80/ai";
 
 export const token = localStorage.getItem('user-visited-dashboard');
