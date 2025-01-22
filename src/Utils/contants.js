28a618875727166fcfb3522202666e6a1ed33929
MuhammadHassan125 <hassanalirajput2004@gmail.com>
2025-01-22 18:10:02 +0500

    ip again change

--- src/Utils/contants.js
+++ src/Utils/contants.js
@@ -1,8 +1,9 @@
 // local ip
 // export const baseURL = "http://192.168.18.196:8001/api";
+// export const AnalyzeURL = "http://87.106.188.142:80/ai";
 
 // server ip
-export const baseURL = "http://87.106.188.142:80/api";
-export const AnalyzeURL = "http://87.106.188.142:80/ai";
+export const baseURL = "https://mycareermap.ai/api";
+export const AnalyzeURL = "https://mycareermap.ai/ai";
 
 export const token = localStorage.getItem('user-visited-dashboard');
